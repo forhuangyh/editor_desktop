7d6b5bb38ec6d5cb39587aadc26d150c9af73951
chenjiatan <chenjiatan@readoneer.com>
2025-09-17 10:36:11 +0800

    文件打开时增加编码,\r处理

--- xc_common/file_utils.py
+++ xc_common/file_utils.py
@@ -2,7 +2,7 @@ import os
 import functions
 from pathlib import Path
 import shutil
-import filecmp
+import chardet
 
 
 def copy_file(platform, src_file_path, dst_dir):
@@ -25,4 +25,57 @@ def copy_file(platform, src_file_path, dst_dir):
         dst_path.unlink()  # 删除目标文件
     shutil.copy2(src_file_path, dst_dir_path)
 
+    save_as_utf(dst_dir_path)
+
     return dst_dir_path
+
+
+def save_as_utf(file_with_path, encoding='utf-8'):
+    """
+    Read contents of a text file to a single string,
+    detecting the encoding automatically.
+    """
+    try:
+        encoding = None
+        read_len = 8192
+        # Use binary mode to read the file's content
+        with open(file_with_path, 'rb') as f:
+            raw_data = f.read(read_len)
+
+        # Detect the encoding
+        result = chardet.detect(raw_data)
+        encoding = result['encoding']
+
+        if encoding == "utf-8":
+            text = raw_data.decode(encoding, errors='replace')
+            if text.find("\r") > -1:
+                # 包含\r, 通常是windows下的文件, 转换为unix格式
+                with open(file_with_path, 'r') as f:
+                    text = f.read()
+                # 包含\r, 通常是windows下的文件, 转换为unix格式
+                text = text.replace("\r", "")
+                # Write the decoded text back to the file, using UTF-8 encoding
+                with open(file_with_path, 'w', encoding='utf-8') as f:
+                    f.write(text)
+            return encoding
+
+        # If a valid encoding is found, decode the data
+        if encoding:
+            with open(file_with_path, 'rb') as f:
+                raw_data = f.read()
+            # Decode the raw data using the detected encoding, with error handling
+            text = raw_data.decode(encoding, errors='replace')
+            if text.find("\r") > -1:
+                # 包含\r, 通常是windows下的文件, 转换为unix格式
+                text = text.replace("\r", "")
+            # Write the decoded text back to the file, using UTF-8 encoding
+            with open(file_with_path, 'w', encoding='utf-8') as f:
+                f.write(text)
+            return encoding
+        else:
+            # If chardet couldn't find a valid encoding, return an empty string
+            return ""
+
+    except Exception as ex:
+        # Catch any other potential errors and print a message
+        raise Exception(f"无法识别的编码: encoding={encoding}, {str(ex)}")
